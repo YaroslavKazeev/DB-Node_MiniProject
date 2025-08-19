bceadf7ae36b5aec4f0e2197797eb8785b84ce8c
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-19 16:42:49 +0200

    acceptence file update

--- .hyf/acceptance.test.js
+++ .hyf/acceptance.test.js
@@ -1,49 +1,54 @@
-import request from 'supertest';
-import app from '../app';
+import request from "supertest";
+import app from "../app";
 
-const testUser = {
-  email: 'testuser@example.com',
-  password: '5f30feYP730QWtl6'
-}
+const getTestUser = () => ({
+  email: `${crypto.randomUUID()}@example.com`,
+  password: crypto.randomUUID(),
+});
 
-describe('Acceptance Tests', () => {
-  it('Acceptance Test - Register ', async () => {
+describe("Acceptance Tests", () => {
+  it("Acceptance Test - Register ", async () => {
+    const testUser = getTestUser();
     const response = await request(app)
-      .post('/users/register')
+      .post("/users/register")
       .send(testUser)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .expect(201);
 
-    expect(response.body).toHaveProperty('id');
-    expect(response.body).toHaveProperty('email', testUser.email);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("email", testUser.email);
   });
 
-  it('Acceptance Test - Login', async () => {
+  it("Acceptance Test - Login", async () => {
     // First create a test user
-    await request(app).post('/users/register').send(testUser).expect(201);
-    
+    const testUser = getTestUser();
+    await request(app).post("/users/register").send(testUser).expect(201);
+
     const response = await request(app)
-      .post('/users/login')
+      .post("/users/login")
       .send(testUser)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body).toHaveProperty('token');
+    expect(response.body).toHaveProperty("token");
   });
 
-  it('Acceptance Test - New Item', async () => {
+  it("Acceptance Test - New Item", async () => {
     // First create a test user and login
-    await request(app).post('/users/register').send(testUser).expect(201);
-    const loginResponse = await request(app).post('/users/login').send(testUser);
-    const token = loginResponse.body.token
+    const testUser = getTestUser();
+    await request(app).post("/users/register").send(testUser).expect(201);
+    const loginResponse = await request(app)
+      .post("/users/login")
+      .send(testUser);
+    const token = loginResponse.body.token;
 
     const response = await request(app)
-      .post('/items')
-      .set('Authorization', `Bearer ${token}`)
-      .send({ title: 'Test Item', price: 123.45 })
-      .expect('Content-Type', /json/)
+      .post("/items")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ title: "Test Item", price: 123.45 })
+      .expect("Content-Type", /json/)
       .expect(201);
 
-    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty("id");
   });
 });
