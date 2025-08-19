f4bd4a44659da301794891159fba7c7cb4af5aa8
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-19 12:30:48 +0200

    index.js fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import app from './app.js';
+import app from "./app.js";
 
 const PORT = 7890;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
