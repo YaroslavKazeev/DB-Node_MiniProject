5a61bf625cca05156b00dccc5596f6d7482f5833
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-27 19:00:10 +0200

    test suit added

--- Tests/sample.test.js
+++ Tests/sample.test.js
@@ -1,13 +1,34 @@
-import request from 'supertest';
-import app from '../app';
+import request from "supertest";
+import app from "../app";
 
-describe('POST /user/login', function() {
-  it('responds with error', function(done) {
-    request(app)
-      .post('/users/login')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(500, done)
-      .expect({ error: 'Not implemented' });
+const getTestUser = () => ({
+  email: `${crypto.randomUUID()}@example.com`,
+  password: crypto.randomUUID(),
+});
+
+describe("Created unit tests", () => {
+  it("Test - Register with correct credentials", async () => {
+    const testUser = getTestUser();
+    const response = await request(app)
+      .post("/users/register")
+      .set("Accept", "application/json")
+      .send(testUser)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("email", testUser.email);
+  });
+
+  it("Test - Register with the empty credentials", async () => {
+    const testUser = {};
+    const response = await request(app)
+      .post("/users/register")
+      .set("Accept", "application/json")
+      .send(testUser)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toHaveProperty("error");
   });
 });
