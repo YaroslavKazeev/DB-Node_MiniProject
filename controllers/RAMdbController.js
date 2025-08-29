f569f7c16db6d4ea041c028519b313bbe9c223f2
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-29 20:42:55 +0200

    refactored giveToken

--- controllers/RAMdbController.js
+++ controllers/RAMdbController.js
@@ -32,6 +32,7 @@ class RAMdatabase {
             values: [userID, email, hashedPassword],
           };
           await client.query(addUserQuery);
+          this.db[email] = { userID, hashedPassword };
           return userID;
         } else {
           throw new Error("User's email already exists in the DB");
@@ -50,6 +51,17 @@ class RAMdatabase {
     }
   }
 
+  async validatePassword(password, hashedPassword, userID, email) {
+    const isPasswordCorrect = await compare(password, hashedPassword);
+    if (isPasswordCorrect) {
+      const token = jsonwebtoken.sign(userID, process.env.JWTsecret);
+      this.db[email].token = token;
+      return token;
+    } else {
+      throw new Error("Credentials are invalid");
+    }
+  }
+
   async giveToken(email, password) {
     password = String(password);
     if (client) {
@@ -60,32 +72,14 @@ class RAMdatabase {
       if (result.rows.length !== 0) {
         const [{ user_id: userID, email, hashedpassword: hashedPassword }] =
           result.rows;
-        const isPasswordCorrect = await compare(password, hashedPassword);
-        if (isPasswordCorrect) {
-          const token = jsonwebtoken.sign(userID, process.env.JWTsecret);
-          if (!this.db[email]) {
-            this.db[email] = {};
-          }
-          this.db[email] = { userID, hashedPassword };
-          this.db[email].token = token;
-          return token;
-        } else {
-          throw new Error("Credentials are invalid");
-        }
+        return this.validatePassword(password, hashedPassword, userID, email);
       } else {
         throw new Error("User's email does not exists in the DB");
       }
     } else {
       if (this.db[email]) {
         const { userID, hashedPassword } = this.db[email];
-        const isPasswordCorrect = await compare(password, hashedPassword);
-        if (isPasswordCorrect) {
-          const token = jsonwebtoken.sign(userID, process.env.JWTsecret);
-          this.db[email].token = token;
-          return token;
-        } else {
-          throw new Error("Credentials are invalid");
-        }
+        return this.validatePassword(password, hashedPassword, userID, email);
       } else {
         throw new Error("User's email does not exists in the DB");
       }
